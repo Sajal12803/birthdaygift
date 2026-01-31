09393d01b690c213d378523a8b7ecf14f8ff9950
Sajal12803 <saxenasajal8@gmail.com>
2026-01-31 05:39:05 +0530

    chore: add TS declarations and node types to fix editor errors

---
+++ src/types/declarations.d.ts
@@ -0,0 +1,5 @@
+declare module '@vitejs/plugin-react-swc';
+declare module 'lovable-tagger';
+
+// Provide __dirname for editor type checking (Node types are installed, but some configs may still complain)
+declare const __dirname: string;
